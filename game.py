1e007ab410ea3485c9f6d4f0c2234486f50b62ac
Tom2015 <dfhe2004@yeah.net>
2014-12-22 18:34:24 -0700

    Fixed

--- game.py
+++ game.py
@@ -1,74 +1,55 @@
-from Tkinter import *
 import time
-import turtle
+from Tkinter import *
+
 from IPython import embed
 
 tk = Tk()
 
-
-
-canvas = Canvas(width=1000, height=800, bg='grey')
-canvas.pack()
+c = Canvas(width=800, height=800, bg='grey')
+c.pack()
 tk.title('game')
 
-s1 = canvas.itemcget(1,'state')
-s2 = canvas.itemcget(2,'state')
-pre,cur = None,None
-canvas.itemconfig(pre, state = 'hidden')
-canvas.itemconfig(cur, state = 'normal')
+def on_key_press_right(evt):
+    c.itemconfig(1, state='hidden')
+    c.move(1, 15, 0)
+    c.move(3, 10,0)
+    c.move(2, 5,0)
 
-def on_keypress_right(evt):   
+    c.itemconfig(2, state='normal')
+    c.update_idletasks() 
+    time.sleep(0.25)
 
-    #pre,cur = None,None
-    if s1 == 'normal':
-        pre,cur = 1,2
-    else:
-        pre,cur = 2,1
-    #canvas.move(cur, 10, 0 )
-    #canvas.itemconfig(pre, state = 'hidden')
-    #canvas.move(cur, 10, 0 )
-    #canvas.itemconfig(cur, state = 'normal')
-    canvas.move(cur, 10, 0 )
+    c.itemconfig(2,state='hidden')
+    c.move(2,10,0)
 
-def on_keypress_space(evt):
-    if s1 == 'normal':
-        pre,cur = 1,2
-    else:
-        pre,cur = 2,1
-    canvas.move(cur, 10, -5)
+    c.itemconfig(3, state='normal')
+    c.update_idletasks()
+    
+    time.sleep(0.25)
+    c.itemconfig(3, state='hidden')
+    c.move(3, 5,0)
+
+    c.itemconfig(1,state='normal')
+
+
+    #c.move(1,10,0)
     
 
 
-def on_keypress_left(evt):
-    if s1 == 'normal':
-        pre,cur = 1,2
-    else:
-        pre,cur = 2,1
-    canvas.move(cur, -10, 0)
+c.bind_all('<KeyPress-Right>', on_key_press_right)
 
-        
+image = PhotoImage(file = 'figure_R1.gif')
+c.create_image(80,80, image=image, anchor=NW, state='normal')
 
+image2 = PhotoImage(file = 'figure_Rrun1.gif')
+c.create_image(80,80, image=image2, anchor=NW, state='hidden')
 
-
-def create_images():
-	R1 = PhotoImage(file = 'figure_R1.gif')
-	canvas.create_image(0, 768, image = R1, anchor=NW)
-
-	R2 = PhotoImage(file = 'figure_R2.gif')
-	canvas.create_image(0, 768, image = R2, anchor=NW, state='hidden')
-	return {
-		1: R1,
-		2: R2,
-	}
+image3 = PhotoImage(file = 'figure_Rrun2.gif')
+c.create_image(80,80, image=image3, anchor=NW, state='hidden')
 
 
 
-
-canvas.bind_all('<KeyPress-Right>', on_keypress_right)
-canvas.bind_all('<KeyPress-space>', on_keypress_space)
-canvas.bind_all('<KeyPress-Left>', on_keypress_left)
-
-
-images = create_images()
 
 mainloop()
+
+
