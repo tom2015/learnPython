dc74de61c276b434134382ee313609bdef8ea265
tom <tom@skip.com>
2014-12-29 14:59:51 -0700

    .

---
+++ 14_1230.py
@@ -0,0 +1,63 @@
+from IPython import embed
+from math import *
+
+
+
+
+def find_num(num):
+    #-- case: simplest:2
+    if num==1:  return []
+
+    #-- case: simpler 
+    for i in range(2,num/2):
+        if num%i!=0: continue               #<-- try next
+        sub_result = find_num(num/i)        #<-- we get it, return result
+        sub_result.append(i)
+        return sub_result
+    
+    return [num,]                           #<-- num is prime number and greater than 1
+
+
+
+
+def square(num):
+    print '*'*num
+    for i in range(2,num):
+        print '*{0}*'.format(' '*(num-2))
+    print '*'*num
+   
+
+
+
+        
+        
+
+
+
+
+if __name__=='__main__':
+
+    if 0:
+        num = 4090
+        rs = find_num(num)
+
+        #-- make sure 
+        i = 1
+        for e in rs:
+            i = i * e
+
+        assert(i==num)
+
+        #-- output
+        rs = [ '%s'%e for e in rs ]
+        print '%s = %s'%(num, ' * '.join(rs))
+
+
+    if 1:
+        square(10)
+
+    
+
+
+
+
